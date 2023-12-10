d0f4d943a90763149f7b0552ed9c327ce8c33373
Samuel Fanawopo <sfanawopo@gmail.com>
2023-12-10 18:04:53 +0000

    current

--- 2023/day10/day10.js
+++ 2023/day10/day10.js
@@ -1,1 +1,46 @@
-console.log("Day 10 of Advent of Code 2023");
+let grid = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+];
+
+let startPos = [0, 0];
+
+const day10 = (grid, startPos) => {
+  let seen = new Array(grid.length)
+    .fill()
+    .map(() => new Array(grid[0].length).fill(false));
+
+  let maxDistance = 0;
+
+  const dfs = (row, col, distance) => {
+    if (
+      row < 0 ||
+      row >= grid.length ||
+      col < 0 ||
+      col >= grid[0].length ||
+      grid[row][col] !== 0
+    )
+      return;
+
+    if (seen[row][col]) return;
+
+    seen[row][col] = true;
+
+    maxDistance = Math.max(maxDistance, distance);
+
+    dfs(row - 1, col, distance + 1);
+    dfs(row + 1, col, distance + 1);
+    dfs(row, col - 1, distance + 1);
+    dfs(row, col + 1, distance + 1);
+
+    seen[row][col] = false; // backtrack
+  };
+
+  dfs(startPos[0], startPos[1], 1); // Start with a distance of 1
+
+  console.log(maxDistance);
+};
+
+day10(grid, startPos);
